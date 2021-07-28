37551b812603af22b7a2bb546dcfed08c4ff15e9
Chellbe <rachelestevesb@gmail.com>
2021-07-28 12:51:28 -0300

    created account class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,13 +3,13 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Rachel"
     var cpf: String = "123.456.789-10"
-    private set
+    private set //modificador de visibilidade
 
 }
 
 fun main() {
     val rachel = Pessoa()
 
-    println(rachel.nome)
-    println(rachel.cpf)
+       println(rachel.nome)
+       println(rachel.cpf)
 }
