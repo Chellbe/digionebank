e0ec0180cb93356c427cc6f23525d0550d302118
Chellbe <rachelestevesb@gmail.com>
2021-07-30 16:31:05 -0300

    created tests and report printer

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioAnalista.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Analista
+
+fun main() {
+
+    val joao = Analista(nome = "João Pedro", cpf = "123.456.789-10", salario = 2000.0)
+
+    ImprimeRelatorioFuncionario.imprime(joao)
+
+}
+
