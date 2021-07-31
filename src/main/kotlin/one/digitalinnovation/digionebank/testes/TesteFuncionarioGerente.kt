4443efc8472f18989fc0648c2fefa6728f1d34b3
Chellbe <rachelestevesb@gmail.com>
2021-07-31 08:46:21 -0300

    created tests and report printer

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -1,14 +1,14 @@
 package one.digitalinnovation.digionebank.testes
 
-import one.digitalinnovation.digionebank.Analista
-import one.digitalinnovation.digionebank.Funcionario
 import one.digitalinnovation.digionebank.Gerente
 
 fun main() {
 
-    val maria = Gerente(nome = "Maria Júlia", cpf = "123.456.789-11", salario = 5000.0)
+    val maria = Gerente(nome = "Maria Júlia", cpf = "123.456.789-11", salario = 5000.0, senha = "senha123")
 
     ImprimeRelatorioFuncionario.imprime(maria)
+
+    TesteAutenticacao().autentica(maria)
 
 }
 
